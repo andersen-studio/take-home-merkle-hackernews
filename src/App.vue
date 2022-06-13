c66b7ba0cb692caab5c8a926892dcbac70a0d34d
tobias-work <tobias@andersen.work>
2022-06-13 11:32:01 +0200

    Add utility classes

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,13 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import 'styles/_reset.scss';
+@import 'styles/_fonts.scss';
+@import 'styles/_colors.scss';
+@import 'styles/_sizes.scss';
+@import 'styles/_layout.scss';
 </style>
