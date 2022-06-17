19a903fe723e808aba682e55dead407e66b027c0
tobias-work <tobias@andersen.work>
2022-06-17 15:43:46 +0200

    Convert CSS to SCSS

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -16,36 +16,36 @@
   display: inline-block;
   width: 80px;
   height: 80px;
-}
 
-.lds-ellipsis div {
-  position: absolute;
-  top: 33px;
-  width: 13px;
-  height: 13px;
-  border-radius: 50%;
-  background: #fafafa !important;
-  animation-timing-function: cubic-bezier(0, 1, 1, 0);
-}
+  div {
+    position: absolute;
+    top: 33px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: #fafafa !important;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
 
-.lds-ellipsis div:nth-child(1) {
-  left: 8px;
-  animation: lds-ellipsis1 0.6s infinite;
-}
+    &:nth-child(1) {
+      left: 8px;
+      animation: lds-ellipsis1 0.6s infinite;
+    }
 
-.lds-ellipsis div:nth-child(2) {
-  left: 8px;
-  animation: lds-ellipsis2 0.6s infinite;
-}
+    &:nth-child(2) {
+      left: 8px;
+      animation: lds-ellipsis2 0.6s infinite;
+    }
 
-.lds-ellipsis div:nth-child(3) {
-  left: 32px;
-  animation: lds-ellipsis2 0.6s infinite;
-}
+    &:nth-child(3) {
+      left: 32px;
+      animation: lds-ellipsis2 0.6s infinite;
+    }
 
-.lds-ellipsis div:nth-child(4) {
-  left: 56px;
-  animation: lds-ellipsis3 0.6s infinite;
+    &:nth-child(4) {
+      left: 56px;
+      animation: lds-ellipsis3 0.6s infinite;
+    }
+  }
 }
 
 @keyframes lds-ellipsis1 {
